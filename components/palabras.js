f73fca1f9d47060c18c6656831a632fa27fd9b5b
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-17 10:58:34 -0400

    Create palabras.js

---
+++ components/palabras.js
@@ -0,0 +1,9 @@
+export const palabrasNivel3 = [
+  'casa', 'perro', 'gato', 'árbol', 'flor', 'sol', 'luna', 'estrella',
+  'agua', 'fuego', 'tierra', 'aire', 'libro', 'mesa', 'silla', 'cama',
+  'puerta', 'ventana', 'coche', 'bici', 'tren', 'avión', 'barco', 'pez',
+  'pájaro', 'mano', 'pie', 'ojo', 'nariz', 'boca', 'oreja', 'diente',
+  'pelo', 'brazo', 'pierna', 'dedo', 'uña', 'corazón', 'cerebro', 'hueso',
+  'rojo', 'azul', 'verde', 'amarillo', 'blanco', 'negro', 'rosa', 'naranja',
+  'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez'
+];
