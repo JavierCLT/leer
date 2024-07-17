6327107647fd57306377ca73cf94243c9f6ec82e
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-17 09:16:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import MetodoLectura from '../components/MetodoLectura'
+import '../styles/globals.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Método de Lectura Español</h1>
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold text-center my-8">Método de Lectura Español</h1>
       <MetodoLectura />
     </div>
   )
