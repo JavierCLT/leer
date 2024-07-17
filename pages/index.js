8a7a578a17f48da6806a77d5fc4126d63152570f
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-17 13:42:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import MetodoLectura from '../components/MetodoLectura'
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold text-center my-8">Leyendo sílabas en español</h1>
+      <h1 className="text-3xl font-bold text-center my-8">Leyendo en español</h1>
       <MetodoLectura />
     </div>
   )
