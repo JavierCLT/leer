d176ba01746193c6fa2c95a4b06a66c255d53f82
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-17 09:05:03 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MetodoLectura from '../components/MetodoLectura'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Método de Lectura Español</h1>
+      <MetodoLectura />
+    </div>
+  )
+}
