9f79bc4cccce52a2ccee9bfb3fe9167e1e3d9bd2
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-18 22:49:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MetodoLectura from '../components/MetodoLectura';
 
 const Home = () => {
   return (
     <div>
-      <h1>Leyendo en Español</h1>
+      <h1 style={{ textAlign: 'center', fontFamily: 'Andika Basic', fontSize: '2rem', color: '#333' }}>Leyendo en Español</h1>
       <MetodoLectura />
     </div>
   );
