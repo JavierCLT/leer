7a86d503dd4546d8db214dc450593ea1c26009ba
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-18 23:14:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import MetodoLectura from '../components/MetodoLectura';
 const Home = () => {
   return (
     <div>
-      <h1 style={{ textAlign: 'center', fontFamily: 'Andika Basic', fontSize: '2rem', color: '#333' }}>Leyendo en Español</h1>
+      <h1 className="font-andika">Leyendo en Español</h1>
       <MetodoLectura />
     </div>
   );
