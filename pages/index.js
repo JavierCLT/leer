d8ad3f24e6dc32cdd9632105031639445386be6a
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-07-18 22:39:26 -0400

    Update and rename index.html to index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import MetodoLectura from '../components/MetodoLectura';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Leer en Español</h1>
+      <MetodoLectura />
+    </div>
+  );
+};
+
+export default Home;
