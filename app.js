1db1e057e8b726caafde8cfaf229bdd2d42c8689
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-09-29 22:47:37 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MetodoLectura from '../components/MetodoLectura';
+
+const Home = () => {
+  return (
+    <div>
+      <MetodoLectura />
+    </div>
+  );
+};
+
+export default Home;
